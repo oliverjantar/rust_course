ddc028b6532f749580e0ea4e7506a6e13ad2a791
jiriandras <jiriandras@protonmail.com>
2023-12-02 22:59:36 +0100

    Rewrite client and server to async using tokio

--- homework_8/server/src/main.rs
+++ homework_8/server/src/main.rs
@@ -1,24 +1,20 @@
 mod args;
 mod server_error;
 
-use anyhow::{bail, Result};
+use anyhow::Result;
 use args::Args;
 use clap::Parser;
+use flume::{Receiver, Sender};
+use futures::stream::{self, StreamExt};
 use server_error::ServerError;
 use shared::message::Message;
 use shared::tracing::{get_subscriber, init_subscriber};
-use std::sync::mpsc::{self};
-use std::{
-    collections::HashMap,
-    net::{SocketAddr, TcpListener, TcpStream},
-    sync::{
-        mpsc::{Receiver, Sender},
-        Arc, Mutex,
-    },
-    thread,
-};
-
-fn main() {
+use std::{collections::HashMap, net::SocketAddr, sync::Arc};
+use tokio::net::tcp::OwnedWriteHalf;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
 
     // Setup tracing, default output is stdout.
@@ -27,142 +23,117 @@ fn main() {
         tracing::error!("Error while setting up server. {e}");
     }
 
-    if let Err(e) = start(args) {
+    if let Err(e) = start(args).await {
         tracing::error!("Error while running server. {e}");
     }
 }
 
 /// Starts the server. It will listen for incoming connections and spawn a new thread for each connection.
 /// In a separate thread runs a broadcasting function that will send messages to all connected clients.
-fn start(args: Args) -> Result<()> {
+async fn start(args: Args) -> Result<()> {
     let server = format!("{}:{}", args.host, args.port);
     tracing::info!("Starting server on address {server}...");
 
-    let listener = TcpListener::bind(server).map_err(ServerError::BindError)?;
+    let listener = TcpListener::bind(server).await.map_err(ServerError::Bind)?;
 
-    if listener.set_nonblocking(true).is_err() {
-        bail!(ServerError::NonblockingListener);
-    }
-    let (sender, receiver) = mpsc::channel();
+    let (sender, receiver) = flume::unbounded();
 
-    let clients: Arc<Mutex<HashMap<SocketAddr, TcpStream>>> = Arc::new(Mutex::new(HashMap::new()));
+    let clients: Arc<Mutex<HashMap<SocketAddr, OwnedWriteHalf>>> =
+        Arc::new(Mutex::new(HashMap::new()));
 
-    let broadcast_handle = thread::spawn({
+    let _broadcast_handle = tokio::spawn({
         let clients = clients.clone();
-        || broadcast_messages(clients, receiver)
+        broadcast_messages(clients, receiver)
     });
 
-    #[allow(clippy::type_complexity)]
-    let (error_sender, error_receiver): (Sender<ServerError>, Receiver<ServerError>) =
-        mpsc::channel();
+    loop {
+        match listener.accept().await {
+            Ok((stream, address)) => {
+                let sender = sender.clone();
+                let clients = clients.clone();
 
-    thread::spawn(move || {
-        for received_error in error_receiver {
-            tracing::error!("Error in connection handling: {}", received_error);
-        }
-    });
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(s) => {
-                thread::spawn({
-                    let sender = sender.clone();
-                    let clients = clients.clone();
-                    let error_sender = error_sender.clone();
-                    move || {
-                        if let Err(e) = handle_connection(s, sender, clients) {
-                            let _ = error_sender.send(e);
-                        }
+                tokio::spawn(async move {
+                    if let Err(e) = handle_connection(stream, address, sender, clients).await {
+                        tracing::error!("Error in connection handling: {}", e);
                     }
                 });
-            }
-            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                continue;
             }
             Err(e) => tracing::error!("Encountered network error from Tcp stream: {e}"),
         }
     }
-
-    if broadcast_handle.join().is_err() {
-        bail!(ServerError::BroadcastThreadError)
-    }
-
-    Ok(())
 }
 
 /// Handles a connection from a client.
 /// In a loop it will listen for incoming messages and send them to the broadcasting thread using chanel.
-fn handle_connection(
-    mut stream: TcpStream,
+async fn handle_connection(
+    stream: TcpStream,
+    address: SocketAddr,
     sender: Sender<(SocketAddr, Message)>,
-    clients: Arc<Mutex<HashMap<SocketAddr, TcpStream>>>,
+    clients: Arc<Mutex<HashMap<SocketAddr, OwnedWriteHalf>>>,
 ) -> Result<(), ServerError> {
-    let addr = stream.peer_addr().map_err(ServerError::PeerAddressError)?;
+    let (mut read_half, mut write_half) = stream.into_split();
 
-    clients
-        .lock()
-        .map_err(|_| ServerError::ClientsLockError)?
-        .insert(
-            addr,
-            stream.try_clone().map_err(ServerError::StreamCloneError)?,
-        );
+    tracing::info!("New connection from: {address}");
 
-    tracing::info!("New connection from: {addr}");
+    let clients_count = clients.lock().await.len();
 
-    let clients_count = clients
-        .lock()
-        .map_err(|_| ServerError::ClientsLockError)?
-        .len();
+    Message::send_active_users_msg(&mut write_half, clients_count)
+        .await
+        .map_err(ServerError::SendMessage)?;
 
-    Message::send_active_users_msg(&mut stream, clients_count)
-        .map_err(ServerError::SendMessageError)?;
+    clients.lock().await.insert(address, write_half);
 
-    while let Ok(message) = Message::receive_msg(&mut stream) {
-        tracing::info!("New message from: {addr}");
+    while let Ok(message) = Message::receive_msg(&mut read_half).await {
+        tracing::info!("New message from: {address}");
         sender
-            .send((addr, message))
-            .map_err(ServerError::ChannelSendError)?;
+            .send((address, message))
+            .map_err(ServerError::ChannelSend)?;
     }
 
     // If the client disconnects we remove it from the list of connected clients.
-    remove_client(&clients, &addr);
+    remove_client(&clients, &address).await;
     Ok(())
 }
 
 /// Broadcasts messages to all connected clients.
 /// If a client is disconnected it will be removed from the list of connected clients.
-fn broadcast_messages(
-    clients: Arc<Mutex<HashMap<SocketAddr, TcpStream>>>,
+async fn broadcast_messages(
+    clients: Arc<Mutex<HashMap<SocketAddr, OwnedWriteHalf>>>,
     receiver: Receiver<(SocketAddr, Message)>,
 ) {
-    while let Ok((ip_addr, message)) = receiver.recv() {
-        let mut clients_iter = clients.lock().expect("Failed to lock clients map");
+    while let Ok((ip_addr, ref message)) = receiver.recv() {
+        let mut clients_iter = clients.lock().await;
 
-        let clients_to_remove: Vec<SocketAddr> = clients_iter
-            .iter_mut()
-            .filter(|(client_addr, _)| **client_addr != ip_addr) // Filter out the client with ip_addr
-            .filter_map(|(client_addr, stream)| {
-                if let Err(e) = Message::send_msg(&message, stream) {
-                    tracing::error!(
-                        "Error while broadcasting message to client {client_addr}. Error: {e}"
-                    );
-                    Some(*client_addr)
-                } else {
-                    None
-                }
-            })
-            .collect();
+        let clients_to_remove: Vec<SocketAddr> = stream::iter(
+            clients_iter
+                .iter_mut()
+                .filter(|(client_addr, _)| **client_addr != ip_addr), // Filter out the client that sent the message
+        )
+        .filter_map(|(client_addr, stream)| async move {
+            tracing::debug!("Sending message to {client_addr}");
+            if let Err(e) = Message::send_msg(message, stream).await {
+                tracing::error!(
+                    "Error while broadcasting message to client {client_addr}. Error: {e}"
+                );
+                Some(client_addr)
+            } else {
+                None
+            }
+        })
+        .collect()
+        .await;
 
-        clients_to_remove.iter().for_each(|&addr| {
-            remove_client(&clients, &addr);
-        });
+        // I could use stream::iter and run all removes concurrently, but since the remove_client locks the clients, it will end up running sequentially anyway
+        for addr in clients_to_remove {
+            remove_client(&clients, &addr).await;
+        }
     }
 }
 
-fn remove_client(clients: &Arc<Mutex<HashMap<SocketAddr, TcpStream>>>, ip_addr: &SocketAddr) {
+async fn remove_client(
+    clients: &Arc<Mutex<HashMap<SocketAddr, OwnedWriteHalf>>>,
+    ip_addr: &SocketAddr,
+) {
     tracing::info!("Removing client from list {ip_addr}");
-    clients
-        .lock()
-        .expect("Failed to lock clients map")
-        .remove(ip_addr);
+    clients.lock().await.remove(ip_addr);
 }
